be4966e064717e07e174adbe92f7f4b1c0cf12c8
hamzakhalid14 <hamzakhalidovic@gmail.com>
2025-10-07 20:38:39 +0100

    Page d'ajout d'exercice complète

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,13 @@ const User = require('../models/User');
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.header('Authorization').replace('Bearer ', '');
+        const authHeader = req.header('Authorization');
+        
+        if (!authHeader) {
+            return res.status(401).json({ message: 'No token, authorization denied' });
+        }
+
+        const token = authHeader.replace('Bearer ', '');
 
         if(!token) {
             return res.status(401).json({ message: 'No token, authorization denied' });
